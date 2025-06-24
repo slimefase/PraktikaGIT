8bca070545a3500a66427ef625b94ccc53d90bba
Cloud <dmitria2522@bk.ru>
2025-06-24 19:07:09 +0700

    Добавил 3 класса

---
+++ Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika
+{
+    internal class Client : Person
+    {
+        public List<Order> Orders { get; set; } = new();
+    }
+}
