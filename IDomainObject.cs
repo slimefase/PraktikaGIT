16886ca5a7dba90ec8dce47dbc9feb6e4cfdbef2
Cloud <dmitria2522@bk.ru>
2025-06-24 19:26:14 +0700

    Добавил 2 класса

---
+++ IDomainObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika
+{
+    internal interface IDomainObject
+    {
+        Guid Id { get; set; }
+    }
+
+}
